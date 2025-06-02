e9bd4bddbbb5ade15f8f91dd629ccc008369ffb0
Nils Wolters <nilsjanis@icloud.com>
2025-06-02 08:36:55 +0200

    Bugfix in Models

--- backend/inventory/models.py
+++ backend/inventory/models.py
@@ -4,13 +4,14 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     
     class Meta:
         verbose_name_plural = "Categories"
+        unique_together = ['name', 'owner']  # Ein Kategoriename pro Benutzer
     
     def __str__(self):
         return self.name
